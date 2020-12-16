2e82e4c9e6e6ff76b816ed62eb54597c8cde2460
Megnate <1327358992@qq.com>
2020-12-16 20:57:46 +0800

    打印

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Class1
+    {
+        public void print()
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine();
+        }
+    }
+}
